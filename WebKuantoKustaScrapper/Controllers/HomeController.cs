68ca00977e0e4b05a4ddb2e4805bce095cb4e6e4
Francisco Meireles <franciscomeireles-10@hotmail.com>
2020-07-25 06:34:15 +0100

    Commit(25/07/2020) Adicionado: -View "ContactUs" -Margem para se afastar do navbar em todos as views -Disclaimer
    Alterado:
    -Site completamente em português
    -Algum Styling às views

--- WebKuantoKustaScrapper/Controllers/HomeController.cs
+++ WebKuantoKustaScrapper/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace WebKuantoKustaScrapper.Controllers
 			return View();
 		}
 
+		public IActionResult ContactUs()
+		{
+			return View();
+		}
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
